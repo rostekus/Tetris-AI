e0bee22e98b1800c4d3ea6d08133127a08f04343
Rostyslav Mosorov <rostyslavmosorov@gmail.com>
2022-04-19 14:53:42 +0200

    added description

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
-from setuptools import setup, find_packages
+import os
 
-setup(name="myproject", version="1.0", packages=find_packages())
+from setuptools import find_packages
+from setuptools import setup
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + "/requirements.txt"
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+setup(
+    name="Tetris AI",
+    version="1.0.0",
+    description="A self-playing Tetris.",
+    author="Rostyslav Mosorov",
+    author_email="rmosorov@icloud.com",
+    license="MIT License",
+    url="https://github.com/rostekus/Tetris-AI",
+    install_requires=install_requires,
+    packages=find_packages(where="src", exclude="tests*"),
+    package_dir={"": "src"},
+)
